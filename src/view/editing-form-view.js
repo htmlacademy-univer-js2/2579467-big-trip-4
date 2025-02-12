606c3e95205df229ed1b03aa897a981168a70751
animuyr <ryumina194@gmail.com>
2025-02-12 19:32:11 +0700

    Добавила уникальные id и name в компонент формы

--- src/view/editing-form-view.js
+++ src/view/editing-form-view.js
@@ -21,9 +21,11 @@ function editFormViewTemplate(event) {
   const offersTemplate = offers
     .map((offer) => {
       const checked = event.offers.includes(offer.id) ? 'checked' : '';
+      const offerName = offer.title.toLowerCase().split(' ').join('-');
+
       return `<div class="event__offer-selector">
-        <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" ${checked}>
-        <label class="event__offer-label" for="event-offer-comfort-1">
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offerName}-${offer.id}" type="checkbox" name="event-offer-${offerName}" ${checked}>
+        <label class="event__offer-label" for="event-offer-${offerName}-${offer.id}">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
