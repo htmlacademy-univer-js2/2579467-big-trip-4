329c7bfdd330c8ed4b0f177971da4ad536500460
animuyr <ryumina194@gmail.com>
2025-03-03 21:21:11 +0700

    Исправила опечатку

--- src/view/editing-form-view.js
+++ src/view/editing-form-view.js
@@ -90,7 +90,7 @@ function editFormViewTemplate(event) {
                 </header>
                 <section class="event__details">
                   <section class="event__section  event__section--offers">
-                    ${offers.length > 0 ? '<h3 class="event__section-title  event__section-title--destination">Offers</h3>' : ''}
+                    ${offers.length > 0 ? '<h3 class="event__section-title  event__section-title--offers">Offers</h3>' : ''}
                     <div class="event__available-offers">
                     ${offersTemplate}
                     </div>
